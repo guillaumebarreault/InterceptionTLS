3675c982a33aff3b3f473415195e853cd59671ba
guillaumebarreault <guillaume.barreault@isen.yncrea.fr>
2022-04-15 12:47:33 -0400

    update

--- Interception_TLS/request_client/requestCert.sh
+++ Interception_TLS/request_client/requestCert.sh
@@ -6,9 +6,10 @@ openssl genrsa -out customCA_$1.key
 # generate crs:
 openssl req -new -key customCA_$1.key -out customCA_$1.csr
 
-
 # CA side: 
 cp customCA_$1.csr ../CA
+rm -fv customCA_$1.csr
+chmod 400 customCA_$1.key
 cd ../CA
 
 # check request:
@@ -17,4 +18,6 @@ openssl req -text -noout -verify -in customCA_$1.csr | grep Subject:
 # sign and generate cert:
 openssl ca -config ca-ssl.conf -out customCA_$1.crt -in customCA_$1.csr
 
+# remove request:
+rm -f customCA_$1.csr
 
